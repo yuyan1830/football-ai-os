0acb753a461836b48a639fa0ec8bae72ab3b5e1a
yuyan1830 <yuyan1830@gmail.com>
2026-07-29 17:27:54 +0800

    V3.2.2 MODEL_LAYER Fusion Engine Integration PASS

--- 05_MODEL_AI/MODEL_LAYER/fusion/fusion_engine.py
+++ 05_MODEL_AI/MODEL_LAYER/fusion/fusion_engine.py
@@ -1,94 +1,46 @@
-
-# -*- coding: utf-8 -*-
-
-
+﻿# -*- coding: utf-8 -*-
 
 class FusionEngine:
 
-
-
     def __init__(self):
 
-        self.weights={
+        self.weights = {
 
+            "Elo":0.25,
 
-            "Elo":
+            "Dixon-Coles":0.25,
 
-            0.25,
+            "Poisson":0.25,
 
-
-            "Dixon-Coles":
-
-            0.25,
-
-
-            "Poisson":
-
-            0.25,
-
-
-            "XGBoost":
-
-            0.25
-
+            "XGBoost":0.25
 
         }
 
 
+    def fuse(self,predictions):
 
-    def fuse(
-        self,
-        predictions
-    ):
-
-
-        home=0
-
-        draw=0
-
-        away=0
-
+        home = 0
+        draw = 0
+        away = 0
 
 
         for model,data in predictions.items():
 
+            weight=self.weights.get(model,0)
 
-            weight=self.weights.get(
+            home += data.get("home_win",0)*weight
 
-                model,
+            draw += data.get("draw",0)*weight
 
-                0
-
-            )
-
-
-            home += data["home_win"] * weight
-
-            draw += data["draw"] * weight
-
-            away += data["away_win"] * weight
-
+            away += data.get("away_win",0)*weight
 
 
         return {
 
+            "home_win":round(home,4),
 
-            "home_win":
+            "draw":round(draw,4),
 
-            round(home,4),
-
-
-            "draw":
-
-            round(draw,4),
-
-
-            "away_win":
-
-            round(away,4)
-
+            "away_win":round(away,4)
 
         }
-
-
-
