5d1d667c060ec85f9ed4f7418d518bba0a4cf3ac
yuyan1830 <yuyan1830@gmail.com>
2026-07-30 21:44:02 +0800

    Checkpoint-062 Output Service V1.0

---
+++ 13_OUTPUT_SERVICE_LAYER/report_service.py
@@ -0,0 +1,59 @@
+﻿# -*- coding: utf-8 -*-
+
+"""
+Football AI OS
+Output Service V1.0
+"""
+
+
+class ReportService:
+
+
+    def generate(self, prediction):
+
+
+        return {
+
+
+            "system":
+                "Football AI OS",
+
+
+            "match":
+                prediction.get(
+                    "match",
+                    {}
+                ),
+
+
+            "analysis":
+                {
+
+
+                "model_result":
+                    prediction.get(
+                        "model_result",
+                        {}
+                    ),
+
+
+                "decision":
+                    prediction.get(
+                        "decision",
+                        {})
+
+                },
+
+
+            "runtime":
+                prediction.get(
+                    "runtime",
+                    {}
+                ),
+
+
+            "status":
+                "REPORT_READY"
+
+        }
+
---
+++ 13_OUTPUT_SERVICE_LAYER/test/report_test.py
@@ -0,0 +1,58 @@
+﻿import sys
+
+
+sys.path.insert(
+    0,
+    r"E:\football_v\13_OUTPUT_SERVICE_LAYER"
+)
+
+
+from report_service import ReportService
+
+
+
+service = ReportService()
+
+
+
+data = {
+
+
+"match":
+{
+"home":"Manchester City",
+"away":"Liverpool"
+},
+
+
+"model_result":
+{
+"home_win_probability":0.42,
+"draw_probability":0.2225,
+"away_win_probability":0.3575
+},
+
+
+"decision":
+{
+"decision":"主胜",
+"confidence":0.42
+}
+
+
+}
+
+
+
+result = service.generate(
+    data
+)
+
+
+print("==============================")
+print("OUTPUT SERVICE TEST")
+print("==============================")
+
+
+print(result)
+
